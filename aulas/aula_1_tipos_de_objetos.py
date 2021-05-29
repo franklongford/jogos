8c3303b56fd39e86c8f00ed638fe51da13205a3c
franklongford <franklongford@gmail.com>
2021-05-29 15:36:37 +0100

    DOC: Adiciona documentos iniciais das aulas (#2)
    * DEV: primeira aula
    
    * DOC: editos menores
    
    * DOC: correcoes da aula primeria

---
+++ aulas/aula_1_tipos_de_objetos.py
@@ -0,0 +1,31 @@
+# Primeira aula de Python: como definir objetos diferents
+# -------------------------------------------------------
+#
+# Linhas que comecam com um simbolo '#' sao comentarios - nao vao funcionar no programa
+# e sao apenas usados pra explicar o codigo.
+#
+# Em Python, voce pode usar letras com acentos ou outros simbolos, mas em geral isso nao
+# e comum porque a lingua usa palavras em ingles.
+
+# Pra definir um numero inteiro.
+um_inteiro = 1
+
+# Pra definir um numero decimal. Eles sao um pouco defierentes do interios
+# mas voce pode criar eles em a mesma maneira
+um_decimal = 2.5
+
+# Pra definir alguma texto curto (so um linha)
+texto_curto = "Oi tudo mundo!"
+
+# Pra definir alguma texto longo (muitas linhas)
+texto_longo = """
+Luisa: Oi gente, tudo bem? Cade voces?
+Lara: To aqui e to pronta pra nossa aula! Mas cade Gael e Joao?
+Luias: Hmmm... acho que eles ainda estao brincando...
+Gael: Eu sou Spiderman!
+Joao: Ola meus legos!
+"""
+
+# Pra definir uma lista das objetos. Uma lista pode ter uma mistura
+# de tipos de objetos (textos e numeros)
+uma_lista = ['mele', 'banana', 5, 7.5]
